cb9f79f88daccb73ddb5ad5db2141c024d73098e
xuwei-k <6b656e6a69@gmail.com>
2017-08-04 11:33:43 +0900

    sbt 0.13.16

--- src/main/scala/sonatype/Sonatype.scala
+++ src/main/scala/sonatype/Sonatype.scala
@@ -20,7 +20,7 @@ object Sonatype {
   }
 
   def run0(profileName: String, commands: List[String]) = {
-    val sbtVersion = "0.13.15"
+    val sbtVersion = "0.13.16"
     val launcher = Path.userHome / ".sbt/launchers" / sbtVersion / "sbt-launch.jar"
     if (!launcher.isFile) {
       val launcherURL = url(
